63ba7ba2a564df1a3f9779586eb2c58151e18d4b
John Labovitz <johnl@johnlabovitz.com>
2024-06-04 11:39:55 -0400

    Update/expand tests.

--- test/test.rb
+++ test/test.rb
@@ -9,13 +9,20 @@ require 'midi-mmc'
 class MIDI::MMC::Test < Minitest::Test
 
   def test_simple
-    io = StringIO.new
-    mmc = MIDI::MMC.new(output: io)
-    mmc.play
-    io.rewind
-    output = io.read.unpack('C*')
-    expected_output = [0xF0, 0x7F, 0x7F, 0x06, 0x02, 0xF7]
-    assert { output == expected_output }
+    mmc = MIDI::MMC.new
+    steps = [
+      [ proc { mmc.reset            }, 'F0 7F 7F 06 0D F7'                          ],
+      [ proc { mmc.goto_zero        }, 'F0 7F 7F 06 44 06 01 00 00 00 00 00 F7'     ],
+      [ proc { mmc.arm_tracks(1)    }, 'F0 7F 7F 06 40 06 4F 05 20 00 00 00 00 F7'  ],
+      [ proc { mmc.stop             }, 'F0 7F 7F 06 01 F7'                          ],
+      [ proc { mmc.arm_tracks(nil)  }, 'F0 7F 7F 06 40 06 4F 05 00 00 00 00 00 F7'  ],
+      [ proc { mmc.goto_zero        }, 'F0 7F 7F 06 44 06 01 00 00 00 00 00 F7'     ],
+      [ proc { mmc.play             }, 'F0 7F 7F 06 02 F7'                          ],
+    ]
+    steps.each do |step, expected_output|
+      output = mmc.message_to_str(step.call)
+      assert { output == expected_output }
+    end
   end
 
 end
